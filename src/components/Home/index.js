7bb58a789309d3bd526c0f618f4145d627c1de05
Prem Raj <prem.raj@bobble.ai>
2022-06-16 20:07:15 +0530

    new commit

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -40,7 +40,7 @@ const Home = () => {
           </h1>
           <h2>Author- Paradoxon: all being sane, none rational</h2>
           <Link to="/contact" className="flat-button">
-            CONTACT HER
+            Wish Her a Happy Birthday
           </Link>
         </div>
         <Logo />
