5d702a0b70f8c85ed3b418bfffdf225682bd35db
Prem Raj <prem.raj@bobble.ai>
2022-06-16 22:27:39 +0530

    minor changes

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -3,8 +3,9 @@ import Loader from "react-loaders";
 import AnimatedLetters from "../AnimatedLetters";
 import "./index.scss";
 import portfolioData from '../../data/paintings.json';
+import Carousel from 'react-bootstrap/Carousel'
 
-const Portfolio = () => { 
+const Portfolio = () => {
     const [letterClass, setLetterClass] = useState('text-animate');
     console.log(portfolioData);
 
@@ -16,33 +17,30 @@ const Portfolio = () => {
         return () => {
             clearTimeout(timer);
         }
-    }); 
+    });
 
 
     const renderPortfolio = (portfolio) => {
         return (
-            <div className="images-container">
-                {
-                    portfolio.map((port, idx) => {
-                        return (
-                            <div className="image-box" key={idx}>
-                                <img 
-                                src={port.cover}
-                                className="portfolio-image"
-                                alt="portfolio" />
-                                <div className="content">
-                                    <p className="title">{port.title}</p>
-                                    <h4 className="description">{port.description}</h4>
-                                    <button
-                                        className="btn"
-                                        onClick={() => window.open(port.url)}
-                                    >View</button>
-                                </div>
-                            </div>
-                        )
-                    })
-                }
-            </div>
+            <AnimatedLetters
+                letterClass={letterClass}
+                strArray={"Loading...".split("")}
+                idx={15}
+            />
+            // <Carousel fade>
+            //     {
+            //         portfolio.map((port, idx) => {
+            //             return (
+            //                 <Carousel.Item>
+            //                     <img
+            //                         src={port.cover}
+            //                         className="portfolio-image"
+            //                         alt="portfolio" />
+            //                 </Carousel.Item>
+            //             )
+            //         })
+            //     }
+            // </Carousel>
         );
     }
 
@@ -53,7 +51,7 @@ const Portfolio = () => {
                 <h1 className="page-title">
                     <AnimatedLetters
                         letterClass={letterClass}
-                        strArray={"Portfolio".split("")}
+                        strArray={"Art Gallery".split("")}
                         idx={15}
                     />
                 </h1>
